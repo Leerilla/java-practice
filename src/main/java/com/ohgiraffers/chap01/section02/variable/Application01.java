553c4ad3c4c3c84b9c06ba694a2e96aba7aa74bb
hwang <hjeonghan0@gmail.com>
2024-04-13 21:09:44 +0900

    meesion

--- src/main/java/com/ohgiraffers/chap01/section02/variable/Application01.java
+++ src/main/java/com/ohgiraffers/chap01/section02/variable/Application01.java
@@ -1,9 +1,16 @@
 package com.ohgiraffers.chap01.section02.variable;
 
+import java.awt.*;
+
 public class Application01 {
     public static void main(String[] args){
         // point 변수에 10의 값을 할당하고 화면에 출력해주세요
         // 출력 예시) 10
         System.out.println(10);
+
+        int Point = 10;
+
+
+        System.out.println(Point +=10);
     }
 }
