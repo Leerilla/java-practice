71d0025bd2f207e7178034f971ce7ba7642f657a
ppudding3861 <ppuding3861@gmail.com>
2024-04-13 22:34:52 +0900

    mission

--- src/main/java/com/ohgiraffers/chap01/section01/literal/Application01.java
+++ src/main/java/com/ohgiraffers/chap01/section01/literal/Application01.java
@@ -4,5 +4,13 @@ public class Application01 {
     public static void main(String[] args){
         // 즐거운 자바를 화면에 출력해주세요
         // 출력예시) 즐거운 자바
+
+        String str1 = "즐거운";
+        String str2 = "자바";
+
+
+        System.out.println(str1 + " " + str2);
+
+
     }
 }
