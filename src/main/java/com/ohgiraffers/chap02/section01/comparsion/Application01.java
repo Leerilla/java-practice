48c6e8a3ea94ca4b5bddb5992207a2ce7cb6a3fd
kjs <wanns2085@gmail.com>
2024-04-16 20:07:30 +0900

    mission 2

--- src/main/java/com/ohgiraffers/chap02/section01/comparsion/Application01.java
+++ src/main/java/com/ohgiraffers/chap02/section01/comparsion/Application01.java
@@ -1,21 +1,35 @@
 package com.ohgiraffers.chap02.section01.comparsion;
 
 public class Application01 {
+    public static void main(String[] args) {
+
+        Application01 app1 = new Application01();
+
+        int x = 5;
+        int y = 5;
+
+        System.out.println(app1.valueCheck(x,y));
+        System.out.println(app1.maxNumberOf(x,y));
+        System.out.println(app1.rangeNum(x));
+
+
+
+    }
 
     // 두 수의 값을 비교하여 같으면 true 다르면 false를 출력하는 프로그램을 작성해주세요
     public boolean valueCheck(int x, int y){
 
-        return true;
+        return (x == y)?true:false;
     }
 
     // 두 수의 값을 비교하여 큰 값을 반환하는 프로그램을 작성해주세요
     // 만약 같다면 0의 값을 반환합니다.
     public int maxNumberOf(int x, int y){
-        return  0;
+        return  (x==y)?0:(x>y)?x:y;
     }
 
     // int x의 값이 0  ~ 100 사이의 값이 맞다면 true 아니면 false를 출력하는 프로그램을 만들어주세요
     public boolean rangeNum(int x){
-        return false;
+        return (x>=0 && x<=100)?true:false;
     }
 }
